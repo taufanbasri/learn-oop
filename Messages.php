4b85f1780fc1764dca9d6f3fafa87861474e3b7c
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2017-10-19 03:30:44 +0000

    Messages

---
+++ Messages.php
@@ -0,0 +1,61 @@
+<?php
+
+class Person
+{
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
+class Business
+{
+    protected $staff;
+
+    public function __construct(Staff $staff)
+    {
+        return $this->staff = $staff;
+    }
+
+    public function hire(Person $person)
+    {
+        $this->staff->add($person);
+    }
+
+    public function getStaffMembers()
+    {
+        return $this->staff->members();
+    }
+}
+
+class Staff
+{
+    protected $members = [];
+
+    public function __construct($members = [])
+    {
+        $this->members = $members;
+    }
+
+    public function add(Person $person)
+    {
+        $this->members[] = $person;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+}
+
+$taufan = new Person('Taufan Prasetyo');
+
+$staff = new Staff([$taufan]);
+
+$itcompany = new Business($staff);
+
+$itcompany->hire(new Person('Evita Karlina'));
+
+var_dump($itcompany->getStaffMembers());
